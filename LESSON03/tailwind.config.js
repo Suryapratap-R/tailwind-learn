4556e53618615ee67ff7902a87ce7e70e86dd5e2
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2023-02-27 21:43:24 +0530

    lesson 3 completed

--- LESSON03/tailwind.config.js
+++ LESSON03/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/**/*.{html, js}'],
+  content: ["./build/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreen: {
+          raw: "(min-aspect-ratio: 3/2)",
+        },
+        tallscreen: {
+          raw: "(min-aspect-ratio: 1/2)",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
